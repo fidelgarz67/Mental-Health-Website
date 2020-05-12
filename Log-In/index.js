0ab05227c10914159503ee72336c69fc8a5fa5bc
Fidel Garcia <fidel67garcia@gmail.com>
2020-05-12 13:46:26 -0600

    Update index.js

--- Log-In/index.js
+++ Log-In/index.js
@@ -1,4 +1,6 @@
 //load the enviorment variables that arent visable (secret word for encryption)
+
+// LogIn / Register Code
 if(process.env.node_env !== 'production'){
     require('dotenv').config()
 }
@@ -17,7 +19,7 @@ var socket = require('socket.io');      //socket for chat
 //allow for passport to verify 
 initialPassport(
     passport, 
-    //find the email and user id in the database
+    //find the email and user id in the database (users array)
     email => users.find(user => user.email === email),
     id => users.find(user => user.id === id)
     )
@@ -25,7 +27,7 @@ initialPassport(
 //users will be stored into an array (needs to be moved into a dataset)
 const users = []
 
-//let the server know that we will be using ejs
+//let the server know of any paths, and or files that are needed
 app.set('view-engine', 'ejs')
 app.use(express.static(path.join(__dirname, 'css')));
 app.use(express.static(path.join(__dirname, 'SessionPageSubscription')));
@@ -36,38 +38,39 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.urlencoded({extended : false}))
 app.use(flash())
 app.use(session({
-    //provide a key (all our user data is safe, (.esv file) per session)
+    //provide a key (all our user data is safe, per session)
     secret:process.env.session_secret,
     resave: false,                  
     saveUninitialized: false
 }))
+
 app.use(passport.initialize())  //initialize the users
-app.use(passport.session())     //keep users persistent between pages
+app.use(passport.session())     //keep users persistent
 app.use(overRide('_method'))    //used for loggin out
 
-//create a path for the ejs files for the server to know
-app.get('/', authenticated, (req, res) =>{
+//create a path for logging out (still need to add to the main)
+app.get('/', (req, res) =>{
     //Go to the home page once signed in
     res.render('index.ejs', {name: req.user.name})
 })
 
 //Log-In 
-app.get('/login', notAuthenticated, (req, res) =>{
+app.get('/login', (req, res) =>{
     res.render('login.ejs')
 });
 
-app.post('/login', notAuthenticated, passport.authenticate('local', {
+app.post('/login', passport.authenticate('local', {
     successRedirect: '/SessionPage/public/selection.html',
     failureRedirect: '/login',
     failureFlash: true              //show error message
 }))
 
 //Register
-app.get('/register', notAuthenticated, (req, res) =>{
+app.get('/register', (req, res) =>{
     res.render('register.ejs')
 })
 
-app.post('/register', notAuthenticated, async (req, res)=>{
+app.post('/register', async (req, res)=>{
     //try to create a user (this needs to moved into a dataset)
     try {
         users.push({
@@ -87,33 +90,13 @@ app.post('/register', notAuthenticated, async (req, res)=>{
     
 })
 
-//Log-Out - Using passport function
+//LogOut - Using passport function
 app.delete('/logout', (req, res) => {
     req.logOut()
     res.redirect('/login')
 })
 
-//ensure that the user is not signed in
-function authenticated(req, res, next){
-    if(req.isAuthenticated()){
-        return next()
-    }
-    res.redirect('/login')
-}
-
-//ensure that the user is signed in
-function notAuthenticated(req, res, next){
-    if(req.isAuthenticated()){
-        return res.redirect('/')
-    }
-    next()
-}
-
-//webpage is on localhost - 2000
-//app.listen(2000)
-
-/****** Chatroom Code ******/
-
+// Chat Room Code
 //Create a server
 var server = app.listen(2000, function(){
     console.log('Listening to request on PORT 2000');    
@@ -121,7 +104,6 @@ var server = app.listen(2000, function(){
 
 //static files for the server to 'see'
 app.use(express.static('/SessionPageSubscription/SessionPage/public'));
-
 
 //Socket Setup
 var io = socket(server);
